000cdf15e08701f5a32c58357ed510d968f22f74
v71ini <94135003+v71ini@users.noreply.github.com>
2021-11-21 20:43:31 +0530

    Create oops9.java

---
+++ oops9.java
@@ -0,0 +1,13 @@
+public class ascii {
+    public static void main(String[] args) {
+        for(int i=65;i<=90;i++){
+            char c= (char) i;
+            System.out.println(c);
+
+        }
+        for (int i=97;i<=122;i++){
+            char c1=(char) i;
+            System.out.println(c1);
+        }
+    }
+}
