5d2218542ebb9b68fd6809d18448044389a01ad9
v71ini <94135003+v71ini@users.noreply.github.com>
2021-11-21 20:46:00 +0530

    Create oops13.java

---
+++ oops13.java
@@ -0,0 +1,25 @@
+public class instances {
+    static int count=0;
+
+    public static void main(String[] args) {
+
+
+    instances obj = new instances();
+    obj.count();
+    instances obj1=new instances();
+    obj1.count();
+    instances obj2=new instances();
+    obj2.count();
+    instances obj3=new instances();
+    obj3.count();
+    instances obj4=new instances();
+    obj4.count();
+    instances obj5=new instances();
+    obj5.count();
+    instances obj6=new instances();
+    obj6.count();
+        System.out.println(count);}
+    void count(){
+        count++;
+    }
+}
